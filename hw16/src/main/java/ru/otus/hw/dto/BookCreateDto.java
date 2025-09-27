bb327a4c4b4487e05671330a68ebda65e079df42
Ash <alex@shintar.ru>
2025-09-28 01:44:54 +0300

    hw16: Copying hw10 as a template

---
+++ hw16/src/main/java/ru/otus/hw/dto/BookCreateDto.java
@@ -0,0 +1,20 @@
+package ru.otus.hw.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record BookCreateDto(
+        @NotBlank(message = "Please provide a title")
+        @Size(min = 2, max = 255, message = "Title must be between {min} and {max} characters")
+        String title,
+
+        @NotNull(message = "Please select an author")
+        Long authorId,
+
+        @NotNull(message = "Please select at least one genre")
+        @Size(min = 1, message = "Please select at least one genre")
+        List<@NotNull(message = "Genre id cannot be null") Long> genreIds) {
+}
