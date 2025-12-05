c15c1954fadf0e294aa13cc6e0d6247b226f2e69
Ash <alex@shintar.ru>
2025-12-05 16:28:44 +0300

    hw18a

---
+++ hw18a/src/main/java/ru/gdemuzei/bot/repositories/MuseumCache.java
@@ -0,0 +1,23 @@
+package ru.gdemuzei.bot.repositories;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.gdemuzei.bot.contracts.MuseumSummaryDto;
+
+@Component
+@RequiredArgsConstructor
+public class MuseumCache {
+
+    private final Cache<String, MuseumSummaryDto> cache;
+
+    public void put(MuseumSummaryDto dto) {
+        if (dto != null && dto.id() != null) {
+            cache.put(dto.id(), dto);
+        }
+    }
+
+    public MuseumSummaryDto get(String id) {
+        return cache.getIfPresent(id);
+    }
+}
